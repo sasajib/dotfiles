28bca81dd51562edea0e6ba555e144bb85203228
Sergiu Puscas <srg.pscs@gmail.com>
2017-04-24 01:20:00 +0300

    removed older i3 config

--- .i3/i3lock.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#scrot /tmp/screen_locked.png
-#convert /tmp/screen_locked.png -blur 400x10 /tmp/screen_locked.png
-i3lock -i /home/sergiu/Pictures/tyler_durden_by_twosaxy-d5fhoao.png
--- .i3/i3suspend.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/home/sergiu/.i3/i3lock.sh
-systemctl suspend
